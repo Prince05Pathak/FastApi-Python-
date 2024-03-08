68605f77867ad1e4fc189a6717b7d4453522e3bb
Prince05Pathak <102548615+Prince05Pathak@users.noreply.github.com>
2024-03-08 18:31:09 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from enum import Enum
+from typing import Optional
 app = FastAPI()
 '''
 @app.get("/")
@@ -46,3 +47,37 @@ async def get_food(food_name: FoodEnum):
         "message": "I like dairy products"
     }
 '''
+#Query Parameters
+fake_items_db = [{"item_name": "Foo"},{"item_name": "For"},{"item_name": "Fro"}]
+
+
+@app.get("/items")
+async def list_items(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip:skip + limit]
+
+
+@app.get("/items/{item_id}")
+async def get_items(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+            item.update(
+            {
+                "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sit."
+            }
+            )
+    return item
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def get_user_items(user_id: int, item_id: str, q: str | None, short: bool = False):
+     item = {"item_id": item_id, "owner_id": user_id}
+     if q:
+         item.update({"q": q})
+     if not short:
+         item.update(
+             {
+                 "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sit."
+             }
+         )
+     return item
